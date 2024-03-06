72b8f935e8b932733d13735c737dcc25f2a8b62e
Esther Kubania <122841197+dev-EstherKubania@users.noreply.github.com>
2024-03-06 13:25:04 +0300

    Create 0-promise.js

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,13 @@
+// 0-promise.js
+function getResponseFromAPI() {
+  const promise = new Promise((resolve) => {
+    // Simulate an asynchronous operation, such as an API call
+    setTimeout(() => {
+      resolve("API response data"); // Resolve the promise with the desired data
+    }, 1000); // Simulating a delay of 1000 milliseconds (1 second)
+  });
+
+  return promise;
+}
+
+export default getResponseFromAPI;
